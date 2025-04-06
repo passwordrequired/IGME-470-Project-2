9d95ca99f017049ef6f61d9795d069e6c05844f5
passwordrequired <jdgray000000@gmail.com>
2025-04-06 17:06:23 -0400

    Reading Off Board
    I've successfully read off the board and sent the values to the C# program. The behavior of the potentiometers is working, but the buttons aren't working correctly

--- input_demonstration/input_demonstration/Program.cs
+++ input_demonstration/input_demonstration/Program.cs
@@ -5,6 +5,8 @@ using System.Linq;
 using System.Text;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Drawing;
+using System.Security.Principal;
 
 namespace input_demonstration
 {
@@ -13,14 +15,32 @@ namespace input_demonstration
         static SerialPort _serialPort;
         public static void Main()
         {
+            string val;
             _serialPort = new SerialPort();
             _serialPort.PortName = "COM5";
             _serialPort.BaudRate = 9600;
             _serialPort.Open();
+            _serialPort.ReadExisting();
             while (true)
             {
-                string a = _serialPort.ReadExisting();
-                Console.WriteLine(a);
+                Console.Clear(); //clears out the previous data
+
+                val = _serialPort.ReadLine(); //prints left click state
+                Console.WriteLine("Left Click: " + val);
+                Thread.Sleep(50);
+
+                val = _serialPort.ReadLine(); //prints right click state
+                Console.WriteLine("Right Click: " + val);
+                Thread.Sleep(50);
+
+                val = _serialPort.ReadLine(); //prints mouse x position
+                Console.WriteLine("Mouse X Position: " + val);
+                Thread.Sleep(50);
+
+                val = _serialPort.ReadLine(); //prints mouse y position
+                Console.WriteLine("Mouse Y Position: " + val);
+                Thread.Sleep(50);
+
                 Thread.Sleep(200);
             }
         }
--- p2-arduino-code/p2-arduino-code.ino
+++ p2-arduino-code/p2-arduino-code.ino
@@ -1,8 +1,27 @@
+int lclick = 7; //left click
+int rclick = 8; //right click
+int xmouse = A1; //mouse x position
+int ymouse = A0; //mouse y position
+
+int input; //holds value to send to C# program
+
 void setup() {
-Serial.begin(9600);
+  Serial.begin(9600); //start the serial communication
 }
 
 void loop() {
-Serial.print("100");
-delay(200);
+  input = digitalRead(lclick);
+  Serial.println(input);
+  delay(50); //to keep in sync with the C# program
+  input = digitalRead(rclick);
+  Serial.println(input);
+  delay(50); //to keep in sync with the C# program
+  input = analogRead(xmouse);
+  Serial.println(input);
+  delay(50); //to keep in sync with the C# program
+  input = analogRead(ymouse);
+  Serial.println(input);
+  delay(50); //to keep in sync with the C# program
+
+  delay(200); //adds a break to make values on vs console readable
 }
