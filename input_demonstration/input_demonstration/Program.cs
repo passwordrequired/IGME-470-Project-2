9332d89da94bfef27fe65d0552efad21ec3dc366
passwordrequired <jdgray000000@gmail.com>
2025-04-06 15:53:11 -0400

    Communication
    Got communication between arduino and visual studio working

---
+++ input_demonstration/input_demonstration/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO.Ports;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace input_demonstration
+{
+    internal class Program
+    {
+        static SerialPort _serialPort;
+        public static void Main()
+        {
+            _serialPort = new SerialPort();
+            _serialPort.PortName = "COM5";
+            _serialPort.BaudRate = 9600;
+            _serialPort.Open();
+            while (true)
+            {
+                string a = _serialPort.ReadExisting();
+                Console.WriteLine(a);
+                Thread.Sleep(200);
+            }
+        }
+    }
+}
---
+++ p2-arduino-code/p2-arduino-code.ino
@@ -0,0 +1,8 @@
+void setup() {
+Serial.begin(9600);
+}
+
+void loop() {
+Serial.print("100");
+delay(200);
+}
